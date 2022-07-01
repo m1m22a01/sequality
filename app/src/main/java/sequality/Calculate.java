13fa4e948fe6ff1845cd64e09b1795daefc3fe77
Mayuko <m1m22a01@oit.ac.jp>
2022-07-01 16:27:46 +0900

    Change App and Calculate and CalculateTest

--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -4,4 +4,43 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public String printSum(int x, int y) {
+    int ans = sum(x, y);
+    double ave = ans / 2;
+
+    return "Sum of " + x + " and " + y + " is " + ans + ". Average is " + ave + ".";
+  }
+
+  public String printAllSum(int start, int end) {
+    int ans = 0;
+    int count = 0;
+    double ave;
+
+    // 合計の計算
+    for (int i = start; i <= end; i++) {
+      ans = sum(ans, i);
+      count++;
+    }
+
+    // 平均の計算
+    ave = (double) ans / count;
+
+    return "Sum of " + start + " to " + end + " is " + ans + ". Average is " + ave + ".";
+  }
+
+  public String printOdd_EvenSum(int start, int end) {
+    int odd_ans = 0, even_ans = 0;
+
+    // 合計の計算
+    for (int i = start; i <= end; i++) {
+      if (i % 2 == 0) {
+        even_ans = sum(even_ans, i);
+      } else {
+        odd_ans = sum(odd_ans, i);
+      }
+    }
+
+    return "Sum of odd " + start + " to " + end + " is " + odd_ans + ". Sum of even is " + even_ans + ".";
+  }
 }
--- app/src/test/java/sequality/CalculateTest.java
+++ app/src/test/java/sequality/CalculateTest.java
@@ -10,4 +10,55 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @Test
+  public void testprintSum() {
+    Calculate calculate = new Calculate();
+
+    // 入力値
+    int x = 2, y = 3;
+
+    // 予測される合計と平均の値
+    int expected_sum = 5;
+    double expected_ave = 2.0;
+
+    String expected = "Sum of " + x + " and " + y + " is " + expected_sum + ". Average is " + expected_ave + ".";
+
+    assertEquals(expected, calculate.printSum(x, y));
+  }
+
+  @Test
+  public void testprintAllSum() {
+    Calculate calculate = new Calculate();
+
+    // 入力値
+    int start = 1, end = 10;
+
+    // 予測される合計と平均の値
+    int expected_sum = 55;
+    double expected_ave = 5.5;
+
+    // 予測される関数の返答
+    String expected = "Sum of " + start + " to " + end + " is " + expected_sum + ". Average is " + expected_ave + ".";
+
+    assertEquals(expected, calculate.printAllSum(start, end));
+  }
+
+  @Test
+  public void testprintOdd_EvenSum() {
+    Calculate calculate = new Calculate();
+
+    // 入力値
+    int start = 1, end = 10;
+
+    // 予測される各合計の値
+    int expected_oddSum = 25;
+    int expected_evenSum = 30;
+
+    // 予測される関数の返答
+    String expected_return = "Sum of odd " + start + " to " + end + " is " + expected_oddSum + ". Sum of even is "
+        + expected_evenSum + ".";
+
+    assertEquals(expected_return, calculate.printOdd_EvenSum(start, end));
+  }
 }
